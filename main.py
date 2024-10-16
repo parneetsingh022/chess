3696538404173ecf389ab41b6ee0cfee84f25a64
parneetsingh022 <parneetsingh022@gmail.com>
2024-10-16 12:22:32 -0700

    Added clock tick to 60

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import pygame
 import sys
+import os
 import constants.colors as colors
 
 from screens.menu import MenuPage
 from screens.board import BoardPage
 
 from utils import screen_manager
+from utils.board_theme_reader import ThemeReader
+
+theme = ThemeReader()
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((650, 650))
 pygame.display.set_caption("Chess")
-
-
 
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
@@ -32,6 +34,9 @@ screen_manager.add_screen("board_page", board_page)
 # Set the initial screen
 screen_manager.set_screen("menu")
 
+# Create a Clock object to control the frame rate
+clock = pygame.time.Clock()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -44,3 +49,6 @@ while True:
     #             current_screen = "settings"
     
         screen_manager.display_current_screen(event)
+    
+    # Control the frame rate
+    clock.tick(60)  # Limit to 60 frames per second
